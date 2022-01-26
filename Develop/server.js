20777b8548d552d48db53f0074a62717d112d9f2
cavazosdotcom <cavazosdotcom@gmail.com>
2022-01-25 18:09:58 -0800

    bare /api/notes post request

--- Develop/server.js
+++ Develop/server.js
@@ -9,11 +9,31 @@ app.use(express.static('public'));
 // 
 app.get('/notes', (req, res) => {
 
-    // send the file `notes.html`
-
+    // SEND THE FILE `notes.html`
+    // res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile();
 });
 
 
+// 
+app.get('/api/notes', (req, res) => {
+
+    // SEND THE FILE `notes.html`
+    res.json( /* send note data */);
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+    // Create (persist) data
+
+    // Access the new note data from 'req'
+
+    // Push it to my existing list of notes
+
+    // Write my updated notes list to the `db.json` file
+    
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
